3927fe9368612e52561162bdc21b81c0a5e51e00
BlackBotZ991 <102890828+BlackBotZ991@users.noreply.github.com>
2022-06-15 21:23:43 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,7 +31,7 @@ global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder i
 global.donasi = "./image/k.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["6282143349069","6282143349069","6282143349069"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Black Ganteng' //sticker wm ubah
+global.packname = '© Black' //sticker wm ubah
 global.author = '082143349069' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
